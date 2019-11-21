450be8bbaa77c3403f545d19d19b5dd74679e9b3
mpavlov <57910640+MihaelPavlov@users.noreply.github.com>
2019-11-21 13:44:22 +0200

    C#-Fundamentals

---
+++ C# Fundamentals/Homework and Labs/0.BasicSyntax and Loops/09 . Padawan Equipment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // номера на предметите равни на номера на студентите
+            // уборудването е  lightsabers,belts, robes
+            // имам сумата която е за уборудването ,колко ученика и цена на всеки предмет/
+            //трябва да сметнем колоко ще му струва
+            //lightsabers се чупат и трябва 10% повече да купим
+            //също всеки 6 ти belts е безплатен.
+
+            double allMoney = double.Parse(Console.ReadLine());
+            double numbersStudents = double.Parse(Console.ReadLine());
+            double priceLightsaber = double.Parse(Console.ReadLine());
+            double priceRobe = double.Parse(Console.ReadLine());
+            double priceBelt = double.Parse(Console.ReadLine());
+
+
+
+            
+            double sabers = Math.Ceiling(numbersStudents * 0.10);
+            double allSabers = priceLightsaber * (sabers+numbersStudents);
+            double allRobes = priceRobe * numbersStudents;
+
+            double belt = numbersStudents;
+            double belst2 = belt;
+            while (belst2 >= 6)
+            {
+                numbersStudents--;
+                belst2 -= 6;
+            }
+            double allBelts = priceBelt * numbersStudents;
+
+
+
+            double endPrice = allSabers + allRobes + allBelts;
+
+
+            if (allMoney>=endPrice)
+            {
+                Console.WriteLine($"The money is enough - it would cost {endPrice:F2}lv.");
+            }
+            else
+            {
+                double neededMoney = endPrice - allMoney;
+                Console.WriteLine($"Ivan Cho will need {neededMoney:F2}lv more.");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Homework and Labs/0.BasicSyntax and Loops/10. Rage Expenses/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // има слушалки , мишка , клавиатура, и монитор
+            //всяка втора загубена игра пешо чупи едни слушалки 
+            //всяка трета загубена игра чупи мишка
+            //когато пешо счупи и мишката и слушалките в същата загубена игра , също таака си чупи и клавиатурата
+            //на  всеки втори път когато си чупи клавиатурата си чупи и монитора
+            //имаме колко игри е загубил и цената на нещата.
+
+
+            int lostGame = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice= double.Parse(Console.ReadLine());
+
+            double money = 0;
+
+            for (int i = 1; i <= lostGame; i++)
+            {
+                if (i % 12 == 0 )
+                {
+                    money += displayPrice;
+                }
+                if (i % 6 == 0)
+                { 
+                    money += keyboardPrice;
+                }
+                if (i % 2 ==0)
+                {
+                    money += headsetPrice;
+                }
+                if (i % 3 ==0)
+                {
+                    money += mousePrice;
+                }
+            }
+            Console.WriteLine($"Rage expenses: {money:f2} lv.");
+
+        }
+    }
+}
