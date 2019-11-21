36499202d7087330a791f727e84ccd3260b0cd4e
mpavlov <57910640+MihaelPavlov@users.noreply.github.com>
2019-11-21 13:45:42 +0200

    add

---
+++ C# Fundamentals/Homework and Labs/1.DataTypes and Variables/0.6 Triples of Latin Letters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._6_Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        char firstChar = (char)('a' + i);
+                        char secondChar = (char)('a' + k);
+                        char thirdhar = (char)('a' + j);
+                        Console.Write(firstChar);
+                        Console.Write(secondChar);
+                        Console.WriteLine(thirdhar);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ C# Fundamentals/Homework and Labs/1.DataTypes and Variables/3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            int course = (int)Math.Ceiling((double)n / p);
+
+            Console.WriteLine(course);
+
+
+        }
+    }
+}
