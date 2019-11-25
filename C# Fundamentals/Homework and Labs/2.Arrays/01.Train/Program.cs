be5dcd0a1cef19b7a3fda28e21abb3d9a7c83e35
mpavlov <57910640+MihaelPavlov@users.noreply.github.com>
2019-11-25 16:06:55 +0200

    Arrays Exercise

---
+++ C# Fundamentals/Homework and Labs/2.Arrays/01.Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int collect = 0;
+
+            for (int i = 0; i < n; i++)
+            {        
+                int num = int.Parse(Console.ReadLine());
+                arr[i] = num;
+                collect += num;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+            Console.WriteLine(collect);
+        }
+    }
+}
