f0cbca874b04a64903dc216eb5c2ddafeb515641
mpavlov <57910640+MihaelPavlov@users.noreply.github.com>
2019-11-25 16:10:10 +0200

    Add.Methods and Functions - Exercise

---
+++ C# Fundamentals/Homework and Labs/3.Methods and Functions/0.7NxN Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _0._7NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                
+                Console.Write(n + " ");
+                for (int j = n; j < i; j++)
+                {
+                    Console.WriteLine(i + " ");
+                }
+
+            }
+        }
+        
+    }
+}
