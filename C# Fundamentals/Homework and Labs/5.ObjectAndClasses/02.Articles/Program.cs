b71da4ad28b52c66a51bdec4ce9e86767e218b49
mpavlov <57910640+MihaelPavlov@users.noreply.github.com>
2019-11-26 19:22:28 +0200

    ADD.ObjectAndClasses-Exercise

---
+++ C# Fundamentals/Homework and Labs/5.ObjectAndClasses/02.Articles/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] read = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            
+            
+            Article article = new Article();
+            
+            article.Edit(read[1]);
+            article.ChangeAuthor(read[2]);
+            article.Rename(read[0]);
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ").ToArray();
+
+                if (command[0] == "Edit")
+                {
+                    article.Edit(command[1]);
+                }
+                else if (command[0]=="ChangeAuthor")
+                {
+                    article.ChangeAuthor(command[1]);
+                }
+                else if (command[0] == "Rename")
+                {
+                    article.Rename(command[1]);
+                }
+              
+
+            }
+            Console.WriteLine(article.ToString());
+            
+            
+        }
+    }
+    class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string newContent)
+        {
+           Content= newContent;
+        }
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+        public void Rename (string newTitle)
+        {
+            Title = newTitle;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
+
---
+++ C# Fundamentals/Homework and Labs/5.ObjectAndClasses/03.Article2.0/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03.Article2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split(" ");
+            var random = new Random();//Read the method
+            var shuffedWords = new List<string>(); // the list where we collect the shuffleWords
+
+            foreach (var word in words) 
+            {
+                int neWIndex =  random.Next(0, shuffedWords.Count + 1);//The suffle
+                shuffedWords.Insert(neWIndex , word);// where we put it
+            }
+            foreach (var word in shuffedWords)
+            {
+                Console.WriteLine(word);// print
+            }
+
+        } 
+
+    }    
+             
+       
+        
+    
+    class Birthday
+    {
+        public int Day  { get; set; }
+        public int Mount { get; set; }
+        public int Year { get; set; }
+
+        public void AddYear()
+        {
+
+        }
+    }
+}
