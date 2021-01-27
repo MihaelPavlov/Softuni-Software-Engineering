5ba36fd72937d892e01e3d5e7e547ed479fd24ae
MihaelPavlov <m.pavlov1405@abv.bg>
2021-01-27 09:14:15 +0200

    Add -> DOM Introduction-Exercise

--- 6.JS Advanced/08.DOM Introduction-Exercise/06. Format the Text/solution.js
+++ 6.JS Advanced/08.DOM Introduction-Exercise/06. Format the Text/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO
+  // split  the text
+  //atleast 1 character
+  //restrict the sentence to 3
+  const getText = document.getElementById("input").value;
+  const outputElement = document.getElementById("output");
+  const splitText = getText.split(".").filter((e) => e != "");
+
+  let result = "";
+
+  for (let index = 0; index < splitText.length; index += 3) {
+    if (splitText[index] != undefined) {
+      result += splitText[index] + ".";
+    }
+    if (splitText[index + 1] != undefined) {
+      result += splitText[index + 1] + ".";
+    }
+    if (splitText[index + 1] != undefined) {
+      result += splitText[index + 2] + ".";
+    }
+
+    let p = document.createElement("p");
+    p.textContent = result;
+    outputElement.appendChild(p);
+    result = "";
+  }
 }
--- 6.JS Advanced/08.DOM Introduction-Exercise/07. Hell's Kitchen/app.js
+++ 6.JS Advanced/08.DOM Introduction-Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,71 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+  document.querySelector("#btnSend").addEventListener("click", onClick);
+  let input = document.querySelector("#inputs>textarea");
+  const bestRestaurantP = document.querySelector("#bestRestaurant>p");
+  const workerP = document.querySelector("#workers>p");
+  function onClick() {
+    const arr = JSON.parse(input.value);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+    const restaurants = {};
+
+    arr.forEach((line) => {
+      const tokens = line.split(" - ");
+
+      const name = tokens[0];
+      const workersArr = tokens[1].split(", ");
+      let workers = [];
+
+      for (const worker of workersArr) {
+        let workerTokens = worker.split(" ");
+        let workerName = workerTokens[0];
+        let workerSalary = workerTokens[1];
+        workers.push({
+          name: workerName,
+          salary: Number(workerSalary),
+        });
+      }
+
+      if (restaurants[name]) {
+        workers = workers.concat(restaurants[name].workers);
+      }
+      workers.sort((a, b) => b.salary - a.salary);
+      let bestSalary = workers[0].salary;
+      let averageSalary =
+        workers.reduce((sum, worker) => sum + worker.salary, 0) /
+        workers.length;
+
+      restaurants[name] = { workers, averageSalary, bestSalary };
+    });
+
+    let bestRestaurantSalary = 0;
+    let best = undefined;
+
+    for (const name in restaurants) {
+      if (restaurants[name].averageSalary > bestRestaurantSalary) {
+        best = {
+          name,
+          workers: restaurants[name].workers,
+          bestSalary: restaurants[name].bestSalary,
+          averageSalary: restaurants[name].averageSalary,
+        };
+
+        bestRestaurantSalary = restaurants[name].averageSalary;
+      }
+    }
+    bestRestaurantP.textContent = `Name: ${
+      best.name
+    } Average Salary: ${best.averageSalary.toFixed(
+      2
+    )} Best Salary: ${best.bestSalary.toFixed(2)}`;
+
+    let workersStringResult = [];
+
+    best.workers.forEach((worker) => {
+      workersStringResult.push(
+        `Name: ${worker.name} With Salary: ${worker.salary}`
+      );
+    });
+
+    workerP.textContent = workersStringResult.join(" ");
+  }
 }
