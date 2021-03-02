362d00654f4d0891a9b1fcaba5516d9dca9c045b
MihaelPavlov <m.pavlov1405@abv.bg>
2021-03-02 14:56:36 +0200

    Add -> Data and Authentication Exercises

--- 7.JS Applications/02.Asynchronous Programming Exercises/03.Forecaster/app.js
+++ 7.JS Applications/02.Asynchronous Programming Exercises/03.Forecaster/app.js
@@ -9,83 +9,104 @@ function attachEvents() {
     console.log(input.value);
     const url = "http://localhost:3030/jsonstore/forecaster/locations";
 
-    const resposne = await fetch(url);
-    const data = await resposne.json();
-    const result = data.filter((x) => x.name == input.value)[0];
-    console.log(result);
-
-    currentCondition(result.code);
-    threeDayForecast(result.code);
+    try {
+      const resposne = await fetch(url);
+      const data = await resposne.json();
+      const result = data.filter((x) => x.name == input.value)[0];
+      console.log(result);
+      currentCondition(result.code);
+      threeDayForecast(result.code);
+  
+    } catch (error) {
+      divCurrentForecats.firstElementChild.textContent='Error'
+    }
+   
   }
 
   async function currentCondition(code) {
-    const url = "http://localhost:3030/jsonstore/forecaster/today/" + code;
-    const resposne = await fetch(url);
-    const data = await resposne.json();
-    console.log(data);
-    const div = createElement("div", undefined, ["class=forecasts"]);
-
-    const symbolSpan = createElement("span", "\u2600", ["class=condition"]);
-    symbolSpan.classList.add("symbol");
-
-    const spanCondition = createElement("span", undefined, ["class=condition"]);
-
-    const spanDataName = createElement("span", data.name, [
-      "class=forecast-data",
-    ]);
-
-    const spanWeather = createElement(
-      "span",
-      `${data.forecast.low}\xB0/${data.forecast.high}\xB0`,
-      ["class=forecast-data"]
-    );
-
-    const spanDataCondition = createElement("span", data.forecast.condition, [
-      "class=forecast-data",
-    ]);
-
-    divCurrentForecats.parentElement.style.display = "block";
-    div.appendChild(symbolSpan);
-    spanCondition.appendChild(spanDataName);
-    spanCondition.appendChild(spanWeather);
-    spanCondition.appendChild(spanDataCondition);
-    div.appendChild(spanCondition);
-    divCurrentForecats.appendChild(div);
-  }
-
-  async function threeDayForecast(code) {
-    const url = "http://localhost:3030/jsonstore/forecaster/upcoming/" + code;
-    const resposne = await fetch(url);
-    const data = await resposne.json();
-
-    const div = createElement("div", undefined, ["class=forecast-info"]);
-
-    data.forecast.forEach((el) => {
-        console.log(el);
-      const spanCondition = createElement("span", undefined, [
-        "class=upcoming",
-      ]);
-
-      const spanSymbol = createElement("span", data.name, [
+   
+    try {
+      const url = "http://localhost:3030/jsonstore/forecaster/today/" + code;
+      const resposne = await fetch(url);
+      const data = await resposne.json();
+      console.log(data);
+      const div = createElement("div", undefined, ["class=forecasts"]);
+  
+      const symbolSpan = createElement("span", "\u2600", ["class=condition"]);
+      symbolSpan.classList.add("symbol");
+  
+      const spanCondition = createElement("span", undefined, ["class=condition"]);
+  
+      const spanDataName = createElement("span", data.name, [
         "class=forecast-data",
       ]);
-
+  
       const spanWeather = createElement(
         "span",
-        `${el.low}\xB0/${el.high}\xB0`,
+        `${data.forecast.low}\xB0/${data.forecast.high}\xB0`,
         ["class=forecast-data"]
       );
-
-      const spanDataCondition = createElement("span", el.condition, [
+  
+      const spanDataCondition = createElement("span", data.forecast.condition, [
         "class=forecast-data",
       ]);
-
-      spanCondition.appendChild(spanSymbol);
+  
+      divCurrentForecats.parentElement.style.display = "block";
+      div.appendChild(symbolSpan);
+      spanCondition.appendChild(spanDataName);
       spanCondition.appendChild(spanWeather);
       spanCondition.appendChild(spanDataCondition);
       div.appendChild(spanCondition);
-    });
-    divUpcomingForecats.appendChild(div)
+      divCurrentForecats.appendChild(div);
+  
+    } catch (error) {
+      divCurrentForecats.firstElementChild.textContent='Error'
+
+    }
+     
+    
+   
+  }
+
+  async function threeDayForecast(code) {
+    try {
+      const url = "http://localhost:3030/jsonstore/forecaster/upcoming/" + code;
+      const resposne = await fetch(url);
+      const data = await resposne.json();
+  
+      const div = createElement("div", undefined, ["class=forecast-info"]);
+  
+      data.forecast.forEach((el) => {
+          console.log(el);
+        const spanCondition = createElement("span", undefined, [
+          "class=upcoming",
+        ]);
+  
+        const spanSymbol = createElement("span", data.name, [
+          "class=forecast-data",
+        ]);
+  
+        const spanWeather = createElement(
+          "span",
+          `${el.low}\xB0/${el.high}\xB0`,
+          ["class=forecast-data"]
+        );
+  
+        const spanDataCondition = createElement("span", el.condition, [
+          "class=forecast-data",
+        ]);
+  
+        spanCondition.appendChild(spanSymbol);
+        spanCondition.appendChild(spanWeather);
+        spanCondition.appendChild(spanDataCondition);
+        div.appendChild(spanCondition);
+      });
+      divUpcomingForecats.appendChild(div)
+    } catch (error) {
+      divCurrentForecats.firstElementChild.textContent='Error'
+      
+    }
+  
   }
 
   function createElement(type, text, attributes = []) {
